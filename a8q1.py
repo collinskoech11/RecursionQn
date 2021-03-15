1d9a007515e23f3f539bd7b34b192297c20cc85b
collinskoech11 <collinskoechck34@gmail.com>
2021-03-15 08:41:01 +0300

    update

--- a8q1.py
+++ a8q1.py
@@ -5,7 +5,7 @@ def rev_sentence(sentence):
     words = sentence.split(' ')
 
     reverse_phrase = ' '.join(reversed(words))
-
+    print("reversed string :\n")
     return reverse_phrase
 
 if __name__ == "__main__":
@@ -13,6 +13,7 @@ if __name__ == "__main__":
     print(rev_sentence(input))
 #REcursive function
 def reverse(str):
+    print("Reversed string")
     if len(str) == 0:
         return str
     else:
