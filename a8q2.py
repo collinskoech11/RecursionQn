ee79c21c0d5af8a9b53f56bf0a66e15d149a41d7
collinskoech11 <collinskoechck34@gmail.com>
2021-03-16 18:09:30 +0300

    fx update

--- a8q2.py
+++ a8q2.py
@@ -1,12 +1,25 @@
 import matplotlib.pyplot as plt
 
 
-initial_population =  int(input("Iitial pupolation"))
-n = int(input("Year in qn"))
-r = float(input("Growth rate in that year"))
-K = int(input("Maximum population the environment can support"))
+initial_population =  int(input("Iitial pupolation \n"))
+n = int(input("No of years \n"))
+r = float(input("Growth rate in that year \n"))
+K = int(input("Maximum population the environment can support \n"))
+current_population = 0
+lst = []
+count = 0
 
-    
+
+def rucursive():
+    for count in range (0,n):
+        for i in range(0,n):
+            current_population = ((initial_population * r) + initial_population)
+            lst.append(curent_population)
+        count ++:
+    else:
+        return 0
+
+print(lst)
 Year = [2001,2002,2003,2004,2006,2007,2008,2009]
 Population = [100,150,200,250,300,350,400,450]  
 plt.plot(Year, Population)
@@ -14,3 +27,8 @@ plt.title('Population Growth Rate ')
 plt.xlabel('Year')
 plt.ylabel('Population THat Year')
 plt.show()
+
+
+print("Span No of years :" , n , "\n")
+print("Population Growth Rate :" , r , "\n")
+print("Max Population :" , k , "\n")
